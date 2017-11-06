20468e4477dc79d1687bf1d0d0fab1b397836429
mr0165061 <mr0165061@otc.edu>
2017-11-06 06:45:01 -0600

    Committing the json files

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Json Reader");
+const _ = require('lodash');
+var jsonfile = require('jsonfile')
+var file = "jsonfiles/data.json";
+
+jsonfile.readFile(file, function(err, obj) {
+    var result = _.mapValues(obj, (value, prop) => {
+        return {prop: prop, value: value };
+    });
+    console.log(result);
+});
