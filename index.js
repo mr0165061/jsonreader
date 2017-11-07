6b9f10337cb5dffc27d50abd070f8ec5358aa320
mr0165061 <mr0165061@otc.edu>
2017-11-06 19:16:25 -0600

    Working code

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const _ = require('lodash');
 var jsonfile = require('jsonfile')
 var file = "jsonfiles/data.json";
 
-jsonfile.readFile(file, function(err, obj) {
-    var result = _.mapValues(obj, (value, prop) => {
-        return {prop: prop, value: value };
+jsonfile.readFile(file, (err, data) => {
+    if (err) {
+        console.error(err);
+    }
+    _.map(data, (x) => {
+        console.log(x);
     });
-    console.log(result);
 });
